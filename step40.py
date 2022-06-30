cc8d3a48bdf9b8cc11d87b0494ed31459c57601a
KOKI UEBO <70581271+koki-u@users.noreply.github.com>
2022-06-30 10:01:08 +0900

    Add files via upload

---
+++ step40.py
@@ -0,0 +1,25 @@
+if '__file__' in globals():
+    import os, sys
+    sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+
+import numpy as np
+from dezero import Variable
+from dezero.utils import sum_to
+import dezero.functions as F
+
+x = np.array([[1, 2, 3], [4, 5, 6]])
+y = sum_to(x, (1, 3))
+print(y)
+
+y = sum_to(x, (2, 1))
+print(y)
+
+x0 = Variable(np.array([1, 2, 3]))
+x1 = Variable(np.array([10]))
+y = x0 + x1
+print(y)
+
+y.backward()
+print(x1.grad)
+
+#これでブロードキャストへの対応も万全！！
